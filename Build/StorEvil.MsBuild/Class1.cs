f2d3d371856d059325a316bba895b3028d22d4a3
Dave Foley <davidmfoley@gmail.com>
2010-04-11 09:20:50 -0700

    adding msbuild task

---
+++ Build/StorEvil.MsBuild/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace StorEvil.MsBuild
+{
+    public class StorEviTask : ITask
+    {
+        public bool Execute()
+        {
+            return true;
+        }
+
+        public string Path { get; set; }
+
+        public IBuildEngine BuildEngine
+        {
+            get; set;
+        }
+
+        public ITaskHost HostObject
+        {
+            get; set;
+        }
+    }
+}
