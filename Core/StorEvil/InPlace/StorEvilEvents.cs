c823a11e86b14c475d87b6fcbc4f225bbfdab6b7
Dave Foley <davidmfoley@gmail.com>
2010-08-01 14:42:05 -0700

    simple event bus

--- Core/StorEvil/InPlace/StorEvilEvents.cs
+++ Core/StorEvil/InPlace/StorEvilEvents.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace StorEvil.InPlace
 {
@@ -20,5 +23,72 @@ namespace StorEvil.InPlace
 
             OnMatchFound(sender, new MatchFoundHandlerArgs { Member = info});
         }
+    }
+
+
+    public class EventBus
+    {
+        private Dictionary<Type, List<object>> _handlers = new Dictionary<Type, List<object>>();
+
+        public void Register(object handler)
+        {
+            var allInterfaces = handler.GetType().GetInterfaces();
+
+            var ifaces = allInterfaces.Where(i => i.Name.StartsWith( "IEventHandler`"));
+
+            foreach (var iface in ifaces)
+            {
+                foreach (var messageType in iface.GetGenericArguments())
+                {
+                    var handlersForType = GetHandlersForType(messageType);
+                    handlersForType.Add(handler);
+                }
+            }
+        }
+
+        private List<object> GetHandlersForType(Type messageType)
+        {
+            if (!_handlers.ContainsKey(messageType))
+                _handlers.Add(messageType, new List<object>());
+
+            return _handlers[messageType];
+        }
+
+        public void Raise<T>(T e)
+        {
+            
+            foreach (var handler in GetHandlersForType(typeof(T)))
+            {
+               
+                var methodInfo = GetHandleMethod<T>(handler);
+                if (methodInfo != null)
+                    methodInfo.Invoke(handler, new object[] {e});
+            }
+        }
+
+        private MethodInfo GetHandleMethod<T>(object handler)
+        {
+            var handleMethods = handler.GetType().GetMethods().Where(m => m.Name == "Handle" &&  m.GetParameters().Count() == 1);
+            var chosenMethod = handleMethods.FirstOrDefault(m => m.GetParameters().First().ParameterType == typeof (T));
+
+            return chosenMethod;            
+        }
+    }
+
+    public interface IEventHandler<T1, T2, T3>
+    {
+        void Handle(T1 eventToHandle);
+        void Handle(T2 eventToHandle);
+        void Handle(T3 eventToHandle);
+    }
+
+    public interface IEventHandler<T1, T2> 
+    {
+        void Handle(T1 eventToHandle);
+        void Handle(T2 eventToHandle);
+    }
+    public interface IEventHandler<T>
+    {
+        void Handle(T eventToHandle);
     }
 }
--- Tests/StorEvil.Tests/Core/StorEvilEvents_Specs.cs
+++ Tests/StorEvil.Tests/Core/StorEvilEvents_Specs.cs
@@ -1,9 +1,134 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 using StorEvil.InPlace;
 using StorEvil.Utility;
 
 namespace StorEvil.Core.Event_Handling
 {
+
+    [TestFixture]
+    public class Event_handling
+    {
+
+        private EventBus Bus;
+        [SetUp]
+        public void SetUpContext()
+        {
+            Bus = new EventBus();
+        }
+
+        [Test]
+        public void can_register_for_an_event()
+        {
+            FooHandler handler = new FooHandler();
+            Bus.Register(handler);
+            var fooEvent = new FooEvent();
+            Bus.Raise(fooEvent);
+            handler.EventsCaught.Count.ShouldEqual(1);
+            handler.EventsCaught.First().ShouldEqual(fooEvent);
+        }
+
+        [Test]
+        public void Can_register_multiple_handlers()
+        {
+            var handler1 = new FooHandler();
+            var handler2 = new FooHandler();
+            Bus.Register(handler1);
+            Bus.Register(handler2);
+            var fooEvent = new FooEvent();
+            Bus.Raise(fooEvent);
+            handler1.EventsCaught.Count.ShouldEqual(1);
+            handler1.EventsCaught.First().ShouldEqual(fooEvent);
+            handler2.EventsCaught.Count.ShouldEqual(1);
+            handler2.EventsCaught.First().ShouldEqual(fooEvent);
+        }
+
+        [Test]
+        public void Raising_an_unhandled_event_does_not_cause_an_error()
+        {
+            var fooEvent = new FooEvent();
+            Bus.Raise(fooEvent);
+        }
+
+        [Test]
+        public void One_handler_can_handle_multiple_events()
+        {
+            var handler = new FooBarBazHandler();
+            Bus.Register(handler);
+            var fooEvent = new FooEvent();
+            var barEvent = new BarEvent();
+            var bazEvent = new BazEvent();
+            Bus.Raise(fooEvent);
+            Bus.Raise(barEvent);
+            Bus.Raise(bazEvent);
+            handler.EventsCaught.ElementsShouldEqual(fooEvent, barEvent, bazEvent);
+        }
+
+        [Test] 
+        public void Multiple_handlers_handler_can_handle_an_event()
+        {
+            var fooBarBazHandler = new FooBarBazHandler();
+            var fooHandler = new FooHandler();
+
+            Bus.Register(fooBarBazHandler);
+            Bus.Register(fooHandler);
+
+            var fooEvent = new FooEvent();
+            var barEvent = new BarEvent();
+            var bazEvent = new BazEvent();
+            
+            Bus.Raise(fooEvent);
+            Bus.Raise(barEvent);
+            Bus.Raise(bazEvent);
+
+            fooBarBazHandler.EventsCaught.ElementsShouldEqual(fooEvent, barEvent, bazEvent);
+            fooHandler.EventsCaught.ElementsShouldEqual(fooEvent);
+        }
+
+        public class FooHandler : IEventHandler<FooEvent>
+        {
+            public List<object> EventsCaught = new List<object>();
+            public void Handle(FooEvent eventToHandle)
+            {
+                EventsCaught.Add(eventToHandle);
+            }
+        }
+    }
+
+    public class FooBarBazHandler : IEventHandler<FooEvent, BarEvent, BazEvent>
+    {
+        public List<object> EventsCaught = new List<object>();
+        public void Handle(FooEvent eventToHandle)
+        {
+            EventsCaught.Add(eventToHandle);
+        }
+
+        public void Handle(BarEvent eventToHandle)
+        {
+            EventsCaught.Add(eventToHandle);
+        }
+
+        public void Handle(BazEvent eventToHandle)
+        {
+            EventsCaught.Add(eventToHandle);
+        }
+    }
+
+    public class BazEvent
+    {
+    }
+
+    public class BarEvent
+    {
+    }
+
+
+    public class FooEvent
+    {
+    }
+
     [TestFixture]
     public class StorEvilEvents_Specs
     {
